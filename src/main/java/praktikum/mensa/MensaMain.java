60df8935505267017e6179e3b7d321257e7b760e
Dmytro Orlov <orlov.dm@gmail.com>
2021-06-19 17:07:58 +0200

    2021-06-19-17-07-58

--- src/main/java/praktikum/mensa/MensaMain.java
+++ src/main/java/praktikum/mensa/MensaMain.java
@@ -1,7 +1,5 @@
 package praktikum.mensa;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
